b6a968c75a9427f89255e908035b833b16b41232
Phyrone <phyrone@phyrone.de>
2018-09-22 16:54:24 +0200

    Init

---
+++ spigot-1.9/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "Downloading BuildTools..."
+wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+echo "Building Spigot"
+java -jar BuildTools.jar --rev 1.9
+fl="$(ls | grep 'spigot-.*')"
+echo "Found $fl"
+cp "$fl" "/spigot.jar"
